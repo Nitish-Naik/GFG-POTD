f07d488825ba4d083a9dcb30bfb798d60938cb46
Nitish Naik <nitishnaik2022@gmail.com>
2024-04-30 20:34:05 +0530

    Add Add_two_numbers_represented_by_linked_lists.java

---
+++ Add_two_numbers_represented_by_linked_lists.java
@@ -0,0 +1,69 @@
+
+class Add_two_numbers_represented_by_linked_lists {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    static Node addTwoLists(Node num1, Node num2) {
+        num1 = reverse(num1);
+        num2 = reverse(num2);
+
+        Node sum = null;
+        int carry = 0;
+
+        while (num1 != null || num2 != null || carry > 0) {
+            int newVal = carry;
+
+            if (num1 != null) {
+                newVal += num1.data;
+                num1 = num1.next;
+            }
+            if (num2 != null) {
+                newVal += num2.data;
+                num2 = num2.next;
+            }
+
+            carry = newVal / 10;
+
+            newVal = newVal % 10;
+
+            Node newNode = new Node(newVal);
+
+            newNode.next = sum;
+
+            sum = newNode;
+        }
+
+        while (sum != null && sum.data == 0) {
+            Node temp = sum.next;
+            sum.next = null;
+            sum = temp;
+        }
+
+        if (sum == null)
+            return new Node(0);
+        return sum;
+    }
+  
+}
